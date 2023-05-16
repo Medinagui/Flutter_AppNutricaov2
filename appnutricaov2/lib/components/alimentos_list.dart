7b984a360538b5a54f4abef8da9460040bd5b97f
Guilherme Medina <gui.castro1906@gmail.com>
2023-05-16 16:09:12 -0300

    avanços em tela Consulta

--- appnutricaov2/lib/components/alimentos_list.dart
+++ appnutricaov2/lib/components/alimentos_list.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:appnutricao/screens/consulta.dart';
 import 'package:appnutricao/themes/theme.dart';
 import 'package:flutter/material.dart';
 import '../components/classes/alimento.dart';
 import '../db/alimentos_database.dart';
-
 
 class AlimentosList extends StatefulWidget {
   const AlimentosList({super.key});
@@ -13,83 +13,115 @@ class AlimentosList extends StatefulWidget {
   State<AlimentosList> createState() => _AlimentosListState();
 }
 
-class _AlimentosListState extends State<AlimentosList> {
 
+class _AlimentosListState extends State<AlimentosList> {
   List<dynamic> listaAlimentos = [];
   bool isLoading = true;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     refreshAlimentos();
     debugPrint('..numero de items: ${listaAlimentos.length}');
   }
 
   Future refreshAlimentos() async {
-
     final data = await SQLHelperAlimentos.getItems();
 
     setState(() {
       isLoading = false;
-      listaAlimentos = data;});
+      listaAlimentos = data;
+    });
   }
 
-    void _deleteItem(int id) async {
+  void _deleteItem(int id) async {
     await SQLHelperAlimentos.deleteItem(id);
     // ignore: use_build_context_synchronously
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Item Deletado com sucesso!')));
+    ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Item Deletado com sucesso!')));
     refreshAlimentos();
   }
 
   @override
   Widget build(BuildContext context) {
-    return
-    Column(
+    return (isLoading)? Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('CARREGANDO...', style: myTextThemes.textTheme.displayLarge,),
+        ],
+      ),
+    )
+    :Column(
       children: [
         //ElevatedButton(onPressed: refreshAlimentos, child: const Text('Atualizar Lista')),
         (listaAlimentos.isEmpty)
-        ? const Center(child: Text('Lista vazia.\nVá para a tela de cadastros\ne cadastre novos alimentos.'),)
-        : SizedBox(
-          height: MediaQuery.of(context).size.height * 0.75,
-          child: Expanded(child: ListView.builder(
-                        itemCount: listaAlimentos.length,
-                        itemBuilder: (context, index) {
-                        final exemplo = listaAlimentos[index];
-        
-                        Image foto = Image.file(File(exemplo['fotoBytes']), fit: BoxFit.cover,);
-                        
-                        return Card(
-                          margin: const EdgeInsets.all(5),
-                          elevation: 5,
-                          child: Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                ConstrainedBox(
-                                constraints: const BoxConstraints(
-                                  maxHeight: 100,
-                                  maxWidth: 100,
-                                  minHeight: 100,
-                                  minWidth: 100
+            ? const Center(
+                child: Text(
+                    'Lista vazia.\nVá para a tela de cadastros\ne cadastre novos alimentos.'),
+              )
+            : Column(
+                children: [
+                  SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.69 - 2.2,
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: listaAlimentos.length,
+                          itemBuilder: (context, index) {
+                            final exemplo = listaAlimentos[index];
+
+                            Image foto = Image.file(
+                              File(exemplo['fotoBytes']),
+                              fit: BoxFit.cover,
+                            );
+
+                            return Card(
+                              margin: const EdgeInsets.all(5),
+                              elevation: 5,
+                              child: Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    ConstrainedBox(
+                                        constraints: const BoxConstraints(
+                                            maxHeight: 100,
+                                            maxWidth: 100,
+                                            minHeight: 100,
+                                            minWidth: 100),
+                                        child:
+                                            //Text('fotoBytes: ${exemplo['fotoBytes']},\nID: ${exemplo['id']}}')),
+                                            ClipOval(child: foto)),
+                                    Flexible(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            exemplo['nome'],
+                                            style: myTextThemes
+                                                .textTheme.labelMedium,
+                                          ),
+                                          Text(
+                                              '${exemplo['tipo']}\n${exemplo['categoria']}',
+                                              style: myTextThemes
+                                                  .textTheme.labelSmall)
+                                        ],
+                                      ),
+                                    ),
+                                    IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.edit)),
+                                    //IconButton(onPressed: () => _deleteItem(exemplo['id']), icon: const Icon(Icons.delete))
+                                  ],
                                 ),
-                                child: 
-                                //Text('fotoBytes: ${exemplo['fotoBytes']},\nID: ${exemplo['id']}}')),
-                                ClipOval(child: foto)),
-                                Flexible(
-                                  child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                                    Text(exemplo['nome'], style: myTextThemes.textTheme.labelMedium,),
-                                    Text('${exemplo['tipo']} - ${exemplo['categoria']}', style: myTextThemes.textTheme.labelSmall)
-                                    ],),
-                                ),
-                                  IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
-                                  //IconButton(onPressed: () => _deleteItem(exemplo['id']), icon: const Icon(Icons.delete))
-                              ],
-                            ),
-                          ),
-                        );
-                      })),
-        ),
+                              ),
+                            );
+                          })),
+                ],
+              ),
       ],
     );
   }
--- appnutricaov2/lib/screens/consulta.dart
+++ appnutricaov2/lib/screens/consulta.dart
@@ -2,6 +2,7 @@ import 'package:appnutricao/components/alimentos_list.dart';
 import 'package:flutter/material.dart';
 
 import '../themes/theme.dart';
+
 class ConsultaScreen extends StatefulWidget {
   const ConsultaScreen({super.key});
 
@@ -9,21 +10,32 @@ class ConsultaScreen extends StatefulWidget {
   State<ConsultaScreen> createState() => _ConsultaScreenState();
 }
 
+int _buttonPressed = 0;
+
+MaterialStateProperty<Color> buttonSelected(int buttonPressed, int buttonID) {
+  if (buttonPressed == buttonID) {
+    return MaterialStateProperty.all(colorsOne.colorScheme.secondary);
+  }
+
+  return MaterialStateProperty.all(colorsTwo.colorScheme.secondary);
+}
+
+List<Widget> listasDisponiveis = const [
+  Text('Lista Usuários'),
+  AlimentosList(),
+  Text('Lista Cardápios'),
+];
+
+List<String> listaOptionsSearch = [
+  'Nome do Usuário',
+  'Nome do Alimento',
+  'Cardápio',
+];
+
 class _ConsultaScreenState extends State<ConsultaScreen> {
-
-  List<String> listaExemplo = [
-    "Exemplo 1",
-    "Exemplo 2",
-    "Exemplo 3",
-    "Exemplo 4",
-    "Exemplo 5",
-    "Exemplo 6",
-    "Exemplo 7",
-    "Exemplo 8",
-  ];
-
   @override
   Widget build(BuildContext context) {
+    final isKeyboard = MediaQuery.of(context).viewInsets.bottom != 0;
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -32,31 +44,110 @@ class _ConsultaScreenState extends State<ConsultaScreen> {
         centerTitle: true,
       ),
       body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-         Expanded(
+        Expanded(
           child: Card(
             margin: const EdgeInsets.fromLTRB(15, 15, 20, 15),
             child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.search),
-                      hintText: 'Insira o nome que deseja procurar',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        borderSide: BorderSide(color: colorsOne.colorScheme.secondary)
-                      )
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    ButtonBar(
+                      alignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _buttonPressed = 0;
+                            });
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: buttonSelected(_buttonPressed, 0),
+                          ),
+                          child: const Text('Usuário'),
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _buttonPressed = 1;
+                              });
+                            },
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    buttonSelected(_buttonPressed, 1)),
+                            child: const Text('Alimento')),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _buttonPressed = 2;
+                              });
+                            },
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    buttonSelected(_buttonPressed, 2)),
+                            child: const Text('Cardápio')),
+                      ],
                     ),
-                  ),
-                  const AlimentosList()
-                ],
-              )
-            ),
+                    // TextField(
+                    //   decoration: InputDecoration(
+                    //       prefixIcon: const Icon(Icons.search),
+                    //       hintText: 'Insira o nome que deseja procurar',
+                    //       border: OutlineInputBorder(
+                    //           borderRadius: BorderRadius.circular(15),
+                    //           borderSide: BorderSide(
+                    //               color: colorsOne.colorScheme.secondary))),
+                    // ),
+                    if (!isKeyboard) listasDisponiveis[_buttonPressed]
+                  ],
+                )),
           ),
         ),
       ]),
       backgroundColor: colorsOne.colorScheme.primary,
+      floatingActionButton: SizedBox(
+        height: 70,
+        width: 70,
+        child: ClipOval(
+          child: Container(
+            color: colorsTwo.colorScheme.secondary,
+            child: IconButton(
+                padding: const EdgeInsets.all(10),
+                onPressed: () {
+                  showModalBottomSheet(
+                      context: context,
+                      builder: ((context) {
+                        return const ModalSearcher();
+                      }));
+                },
+                icon: const Icon(
+                  Icons.search,
+                  size: 40,
+                )),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ModalSearcher extends StatelessWidget {
+  const ModalSearcher({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.search),
+              hintText: listaOptionsSearch[_buttonPressed],
+              border: OutlineInputBorder( 
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide:
+                      BorderSide(color: colorsOne.colorScheme.secondary))),
+        ),
+      ),
     );
   }
 }
